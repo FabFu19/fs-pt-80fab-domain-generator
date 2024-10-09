30ae4d7bbe6f9179a41dfc4a1245bfd75a49aee9
FabFu19 <fabianfuen19@icloud.com>
2024-10-09 17:27:49 +0000

    feat: domain generator created

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const domainGene = document.querySelector("#domain-gen");
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  domainGene.innerHTML = createRandomDomains();
+};
+
+let pronoun = ["the", "our", "a", "its"];
+let adj = ["great", "big", "lovely", "warm"];
+let noun = ["jogger", "racoon", "student", "developer"];
+let dotExtension = [".com", ".es", ".org", ".net", ".4geeks"];
+
+const createRandomDomains = () => {
+  pronoun.forEach(pron =>
+    adj.forEach(adjectiv =>
+      noun.forEach(non =>
+        dotExtension.forEach(extension =>
+          console.log(`${pron}${adjectiv}${non}${extension}`)
+        )
+      )
+    )
+  );
 };
